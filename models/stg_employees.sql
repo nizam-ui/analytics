5e63ef259762edd8545afcdc218a407a1424097b
Nijamudhin S <nizamdbt@gmail.com>
2026-04-30 04:18:40 +0000

    updated

--- models/stg_employees.sql
+++ models/stg_employees.sql
@@ -1,6 +1,30 @@
 {{
     config(
-        materialized = 'table'
+        materialized = 'table',
+        pre_hook = ["truncate table hr.audit", "
+            insert into {{ source('hr', 'audit') }} (
+                model_name,
+                run_id,
+                start_time,
+                end_time,
+                run_by_user 
+            )
+            values(
+                '{{this}}',
+                '{{ invocation_id }}',
+                current_timestamp,
+                null,
+                current_user
+            )
+        "],
+        post_hook = ["
+            update {{ source('hr', 'audit') }}
+            set end_time = current_timestamp
+            where model_name = '{{this}}'
+            and run_id = '{{ invocation_id }}'
+        "]
+
+
         )
 }}
 
@@ -9,6 +33,7 @@ with emp as (
 )
 
 SELECT 
+{{ dbt_utils.generate_surrogate_key(['EMPLOYEE_ID', 'DEPARTMENT_ID']) }} as sk,
 EMPLOYEE_ID,
 FIRST_NAME,
 LAST_NAME,
